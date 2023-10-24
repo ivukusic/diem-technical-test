5ac3902c01765e479823712d02c4a023f091fe7f
jahill <jahill@groupon.com>
2023-10-24 17:11:11 +0100

    chore: [#49] - added basic styles and boilerplate

--- app/root.jsx
+++ app/root.jsx
@@ -5,12 +5,21 @@ export default function App() {
     <html>
       <head>
         <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <link rel="stylesheet" href="./styles.css" />
         <Meta />
         <Links />
       </head>
       <body>
-        <h1>Diem!</h1>
-        <h4>Technical Test</h4>
+        <div className="container">
+          <h1>Diem!</h1>
+          <h4>Technical Test</h4>
+          <p>Please use this file as your entry point.</p>
+          <ul>
+            <li>Demonstrate your approach to componentisation</li>
+            <li>Follow the acceptance criteria thoroughly</li>
+            <li>Use your best judgment on design</li>
+          </ul>
+        </div>
         <Outlet />
         <Scripts />
         <LiveReload />
--- app/server.js
+++ app/server.js
@@ -5,6 +5,7 @@ import express from 'express';
 import * as build from "../build/index.js";
 
 const app = express();
+
 app.use(express.static('public'));
 
 // and your app is "just a request handler"
